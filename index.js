750eccb62a3b983a67218207ca228a6ef14452d1
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-11 15:32:56 -0400

    declaring asynchronous function for prompts

--- index.js
+++ index.js
@@ -19,18 +19,18 @@ con.connect(function (err) {
 
     con.query(sql1, function (err, result) {
         if (err) throw err;
-        console.log("Departmentt Table created");
+        console.log("Department Table Created");
     });
 
 
     con.query(sql2, function (err, result) {
         if (err) throw err;
-        console.log("Role Table created");
+        console.log("Role Table Created");
     });
 
     con.query(sql3, function (err, result) {
         if (err) throw err;
-        console.log("Emplyee Table created");
+        console.log("Employee Table Created");
     });
 
     inquirer.prompt([
@@ -38,7 +38,7 @@ con.connect(function (err) {
             type: "list",
             name: "list",
             message: "Add to Department, Role or Employee",
-            choices: ["Add Employee,Role & Department", "View Employee, Role & Department", "Update Employee Roles"],
+            choices: ["Add Employee, Role & Department", "View Employee, Role & Department", "Update Employee Roles"],
         },
         {
             type: "list",
@@ -50,5 +50,95 @@ con.connect(function (err) {
         {
             type: "input",
             name: "which",
-            message: "Enter Employee ID and Role ID to update",
-            when: (answers) => answers.list == "Update employee roles",
+            message: "Enter Employee ID & Role ID To Update",
+            when: (answers) => answers.list == "Update Employee Roles",
+
+            validate: function (input) {
+                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
+                var done = this.async();
+                setTimeout(function () {
+                    const l = input.split(" ");
+                    var sql = "UPDATE employee SET role_id = " + l[1] + " WHERE id = " + l[0];
+                    con.query(sql, function (err, result) {
+                        if (err) throw err;
+                        console.log("1 Record Updated");
+                    });
+                    done(null, true);
+                    return;
+                }, 100);
+            }
+        },
+
+        {
+            type: "list",
+            name: "View",
+            message: "Select Where You Want To Add",
+            choices: ["Department", "Employee", "Role"],
+            when: (answers) => answers.list == "View Employee, Role & Department"
+        },
+        {
+
+            name: "Department",
+            message: "Enter Name Of Department",
+            when: (answers) => answers.View_What == "Department",
+            validate: function (input) {
+                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
+                var done = this.async();
+
+                setTimeout(function () {
+                    var sql = "SELECT * FROM department";
+                    con.query(sql, function (err, result, fields) {
+                        if (err) throw err;
+                        console.log(result);
+                    });
+                    done(null, true);
+                    return;
+                }, 100);
+            }
+        },
+
+        {
+            name: "Role",
+            message: "Enter Name Of Department",
+            when: (answers) => answers.View_What == "Role",
+            validate: function (input) {
+                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
+                var done = this.async();
+                setTimeout(function () {
+                    var sql = "SELECT * FROM role";
+                    con.query(sql, function (err, result, fields) {
+                        if (err) throw err;
+                        console.log(result);
+                    });
+                    done(null, true);
+                    return;
+                }, 100);
+            }
+        },
+
+        {
+            type: "input",
+            name: "Employee",
+            message: "Enter first_name,last_name,role_id & department_id",
+            when: (answers) => answers.which == "Employee",
+            validate: function (input) {
+                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
+                var done = this.async();
+                setTimeout(function () {
+                    const l = input.split(" ");
+                    var sql = "INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES ('" + l[0] + "','" + l[1] + "'," + l[2] + "," + l[3] + ")";
+                    con.query(sql, function (err, result) {
+                        if (err) throw err;
+                        console.log("1 record inserted");
+                    });
+                    done(null, true);
+                    return;
+                }, 100);
+            }
+        },
+
+    ])
+        .then(answers => {
+            console.info("Answer:", "Done");
+        })
+});

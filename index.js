cf95dfddc0b575b4f5a2034ebef6196ec8a16746
Ankit Somani <ankit.m.somani@gmail.com>
2021-04-24 15:32:55 -0400

    creating switch statements

--- index.js
+++ index.js
@@ -1,144 +1,157 @@
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-const { async } = require('rxjs');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const { async } = require("rxjs");
 
-var con = mysql.createConnection({
-    host: "127.0.0.1",
-    user: "root",
-    password: "password",
-    database: "mydb"
+// var con = mysql.createConnection({
+//   host: "us-cdbr-east-03.cleardb.com",
+//   user: "b0e2c319f73269",
+//   password: "63daf449",
+//   database: "heroku_723b3053db3b507",
+// });
 
-});
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log("Connected!");
+//   var sql1 =
+//     "CREATE TABLE department (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))";
+//   var sql2 =
+//     "CREATE TABLE role (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255),salary DECIMAL(10,2),department_id INT,FOREIGN KEY (department_id) REFERENCES department(id))";
+//   var sql3 =
+//     "CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255),role_id INT,manager_id INT,FOREIGN KEY (role_id) REFERENCES role(id))";
 
-con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql1 = "CREATE TABLE department (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))";
-    var sql2 = "CREATE TABLE role (id INT AUTO_INCREMENT PRIMARY KEY, title VARCHAR(255),salary DECIMAL(10,2),department_id INT,FOREIGN KEY (department_id) REFERENCES department(id))";
-    var sql3 = "CREATE TABLE employee (id INT AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255),role_id INT,manager_id INT,FOREIGN KEY (role_id) REFERENCES role(id))";
+//   con.query(sql1, function (err, result) {
+//     if (err) throw err;
+//     console.log("Department Table Created");
+//   });
 
-    con.query(sql1, function (err, result) {
-        if (err) throw err;
-        console.log("Department Table Created");
-    });
+//   con.query(sql2, function (err, result) {
+//     if (err) throw err;
+//     console.log("Role Table Created");
+//   });
 
+//   con.query(sql3, function (err, result) {
+//     if (err) throw err;
+//     console.log("Employee Table Created");
+//   });
+// });
 
-    con.query(sql2, function (err, result) {
-        if (err) throw err;
-        console.log("Role Table Created");
-    });
+var departments = [{ id: 5567, name: "mathematics" }];
+var roles = [
+    { id: 88891, title: "janitor", salary: 50000, department_id: 5567 },
+];
+var employees = [
+    { id: 13, first_name: "jim", first_name: "smith", role_id: 88891 },
+];
 
-    con.query(sql3, function (err, result) {
-        if (err) throw err;
-        console.log("Employee Table Created");
-    });
+const askInput = () => {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "list",
+                message: "Add to Department, Role or Employee",
+                choices: [
+                    "Add Employee, Role & Department",
+                    "View Employee, Role & Department",
+                    "Update Employee Roles",
+                ],
+            },
+            {
+                type: "list",
+                name: "category",
+                message: "Select Where You'd Like To Add",
+                choices: ["Department", "Employee", "Role"],
+                when: (answers) => answers.list == "Add Employee, Role & Department",
+            },
+            {
+                type: "input",
+                name: "newDepartment",
+                message: "Enter Name of Department",
+                choices: ["Department", "Employee", "Role"],
+                when: (answers) => answers.category == "Department",
+            },
+            {
+                type: "input",
+                name: "newRole",
+                message: "Enter Name of New Role",
+                choices: ["Department", "Employee", "Role"],
+                when: (answers) => answers.category == "Role",
+            },
+            {
+                type: "input",
+                name: "newEmployee",
+                message:
+                    "Enter first_name,last_name,role_id & department_id of new employee",
+                choices: ["Department", "Employee", "Role"],
+                when: (answers) => answers.category == "Employee",
+            },
 
-    inquirer.prompt([
-        {
-            type: "list",
-            name: "list",
-            message: "Add to Department, Role or Employee",
-            choices: ["Add Employee, Role & Department", "View Employee, Role & Department", "Update Employee Roles"],
-        },
-        {
-            type: "list",
-            name: "which",
-            message: "Select Where You'd Like To Add",
-            choices: ["Department", "Employee", "Role"],
-            when: (answers) => answers.list == "Add Employee, Role, & Department"
-        },
-        {
-            type: "input",
-            name: "which",
-            message: "Enter Employee ID & Role ID To Update",
-            when: (answers) => answers.list == "Update Employee Roles",
+            {
+                type: "input",
+                name: "updateEmployee",
+                message: "Enter Employee ID & Role ID To Update",
+                when: (answers) => answers.list == "Update Employee Roles",
+            },
 
-            validate: function (input) {
-                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
-                var done = this.async();
-                setTimeout(function () {
-                    const l = input.split(" ");
-                    var sql = "UPDATE employee SET role_id = " + l[1] + " WHERE id = " + l[0];
-                    con.query(sql, function (err, result) {
-                        if (err) throw err;
-                        console.log("1 Record Updated");
-                    });
-                    done(null, true);
-                    return;
-                }, 100);
+            {
+                type: "list",
+                name: "View",
+                message: "Select what you want to view",
+                choices: ["Department", "Employee", "Role"],
+                when: (answers) => answers.list == "View Employee, Role & Department",
+            },
+        ])
+        .then((answers) => {
+            if (answers.newDepartment) {
+                departments.push({
+                    id: Math.floor(Math.random() * 9999),
+                    name: answers.newDepartment,
+                });
             }
-        },
-
-        {
-            type: "list",
-            name: "View",
-            message: "Select Where You Want To Add",
-            choices: ["Department", "Employee", "Role"],
-            when: (answers) => answers.list == "View Employee, Role & Department"
-        },
-        {
-
-            name: "Department",
-            message: "Enter Name Of Department",
-            when: (answers) => answers.View_What == "Department",
-            validate: function (input) {
-                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
-                var done = this.async();
-
-                setTimeout(function () {
-                    var sql = "SELECT * FROM department";
-                    con.query(sql, function (err, result, fields) {
-                        if (err) throw err;
-                        console.log(result);
-                    });
-                    done(null, true);
-                    return;
-                }, 100);
+            if (answers.newRole) {
+                roles.push({
+                    id: Math.floor(Math.random() * 9999),
+                    title: answers.newRole,
+                });
             }
-        },
-
-        {
-            name: "Role",
-            message: "Enter Name Of Department",
-            when: (answers) => answers.View_What == "Role",
-            validate: function (input) {
-                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
-                var done = this.async();
-                setTimeout(function () {
-                    var sql = "SELECT * FROM role";
-                    con.query(sql, function (err, result, fields) {
-                        if (err) throw err;
-                        console.log(result);
-                    });
-                    done(null, true);
-                    return;
-                }, 100);
+            if (answers.newEmployee) {
+                let params = answers.newEmployee.split(" ");
+                employees.push({
+                    id: Math.floor(Math.random() * 9999),
+                    first_name: params[0],
+                    last_name: params[1],
+                    role_id: params[2],
+                    department_id: params[3],
+                });
             }
-        },
+            if (answers.updateEmployee) {
+                let params = answers.updateEmployee.split(" ");
+                console.log("setting role of employee ", params);
 
-        {
-            type: "input",
-            name: "Employee",
-            message: "Enter first_name,last_name,role_id & department_id",
-            when: (answers) => answers.which == "Employee",
-            validate: function (input) {
-                // DECLARE FUNCTION AS ASYNCHRONOUS & SAVE THE CALLBACK //
-                var done = this.async();
-                setTimeout(function () {
-                    const l = input.split(" ");
-                    var sql = "INSERT INTO employee(first_name,last_name,role_id,manager_id) VALUES ('" + l[0] + "','" + l[1] + "'," + l[2] + "," + l[3] + ")";
-                    con.query(sql, function (err, result) {
-                        if (err) throw err;
-                        console.log("1 record inserted");
-                    });
-                    done(null, true);
-                    return;
-                }, 100);
+                // find the employee by id
+                for (const i in employees) {
+                    let elem = employees[i];
+                    if (elem.id == parseInt(params[0])) {
+                        employees[i].role_id = parseInt(params[1]);
+                    }
+                }
             }
-        },
 
-    ])
-        .then(answers => {
-            console.info("Answer:", "Done");
-        })
-});
+            if (answers.View == "Department") {
+                console.log("\n Departments : \n ");
+                console.table(departments);
+            }
+            if (answers.View == "Role") {
+                console.log("\n Roles : \n ");
+                console.table(roles);
+            }
+            if (answers.View == "Employee") {
+                console.log("\n Employees : \n ");
+                console.table(employees);
+            }
+
+            askInput();
+        });
+};
+
+askInput();
